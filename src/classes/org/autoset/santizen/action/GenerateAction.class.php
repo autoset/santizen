0417150b659cec815b71e828daaf26435438da84
caoy/AutoSet Developer Group <caoy@autoset.org>
2016-06-22 21:51:43 +0900

    generate 액션 클래스 추가(작업 중)

---
+++ src/classes/org/autoset/santizen/action/GenerateAction.class.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace org\autoset\santizen\action;
+
+use org\autoset\santizen\util\ConfigUtil;
+use org\autoset\santizen\util\DatabaseFactory;
+
+class GenerateAction implements Action {
+
+	public function setCommandArguments(array $args) {
+	}
+
+	public function run() {
+	}
+
+}
